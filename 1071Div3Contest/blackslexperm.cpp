650b775bdd54f569c6ad4ea22d837727bbc3fbcd
Akshath Sivachidhambaram <akshath.siva@gmail.com>
2025-12-27 11:23:37 -0600

    solved three questions from 2025 bye contest, with hardest being a prefix and suffix sum trick

--- 1071Div3Contest/blackslexperm.cpp
+++ 1071Div3Contest/blackslexperm.cpp
@@ -9,23 +9,23 @@ int main () {
         std::cin >> n;
         std::set<int> printed = {};
         for (int i =0; i < n; ++i) {
-            int val = std::pow(2, n-i)-1;
+            int val = (1<<(n-i))-1;
             std::cout << val  << " ";
             printed.insert(val);
             for (int j = n-i; j < n-1; ++j) {
                 int curr = n-1-j;
-                for (int p = 1; p < std::pow(2, curr); ++p) {
+                for (int p = 1; p < (1 << curr); ++p) {
                     int shifted =  p<<(j+1);
                     if (printed.find(val+shifted) == printed.end()) {
                         std::cout << (val + shifted) << " ";
                         printed.insert(val+shifted);
                     }
-                }
+                }  
             }
         }
 
-        for (int i = 0; i < pow(2,n); i+=2) {
-            if (i != pow(2,n)-2) {
+        for (int i = 0; i < (2 << n); i+=2) {
+            if (i != (2 << n)-2) {
                 std::cout << i << " ";
             }
             else {
